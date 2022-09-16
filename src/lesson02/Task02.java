6b683c764bfe091c1f2176217d376e85f6dfe06e
Vict Pol <vpolik10@gmail.com>
2022-09-16 16:11:07 +0200

    feat: update Task02 file

--- src/lesson02/Task02.java
+++ src/lesson02/Task02.java
@@ -2,9 +2,9 @@ package lesson02;
 
 public class Task02 {
     public static void main(String[] args) {
-        float circleRadius = 5.73f;
-        final float CONST_PI = 3.14f;
-        System.out.println("Длина окружности равна: " + 2 * CONST_PI * circleRadius);
-        System.out.println("Площадь круга равна: " + CONST_PI * circleRadius * circleRadius);
+        int widthOfRectangel = 10;
+        int heightOfRectangel = 5;
+        System.out.println("Периметр прямоугольника равен: " + (widthOfRectangel + heightOfRectangel)*2);
+        System.out.println("Площадь прямоугольника равна: " + widthOfRectangel*heightOfRectangel);
     }
 }
