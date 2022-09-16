3698af8ec4085d72ceb111c0328a50ee0c091abf
Vict Pol <vpolik10@gmail.com>
2022-09-16 16:05:37 +0200

    feat: update Task02 file

--- src/lesson02/Task02.java
+++ src/lesson02/Task02.java
@@ -1,4 +1,10 @@
 package lesson02;
 
 public class Task02 {
+    public static void main(String[] args) {
+        float circleRadius = 5.73f;
+        final float CONST_PI = 3.14f;
+        System.out.println("Длина окружности равна: " + 2 * CONST_PI * circleRadius);
+        System.out.println("Площадь круга равна: " + CONST_PI * circleRadius * circleRadius);
+    }
 }
