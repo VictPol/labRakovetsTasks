a8234b9ad5ae7ea6d4dac84dc471a389d6a2c485
Vict Pol <vpolik10@gmail.com>
2022-09-16 21:01:30 +0200

    feat: update Task01 file

--- src/lesson04/Task01.java
+++ src/lesson04/Task01.java
@@ -1,4 +1,10 @@
 package lesson04;
 
 public class Task01 {
+    public static void main(String[] args) {
+        // задача - распечатать подряд целые числа от 0 до 10
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
 }
