4886396b35a05b469c7815656f34958ab2b121ce
Vict Pol <vpolik10@gmail.com>
2022-09-16 16:02:09 +0200

    feat: update Task01 file

--- src/lesson02/Task01.java
+++ src/lesson02/Task01.java
@@ -1,4 +1,10 @@
 package lesson02;
 
 public class Task01 {
+    public static void main(String[] args) {
+        int numberDividend = 62;
+        int numberDivider = 15;
+        System.out.println("Результат целочисленного деления: " + numberDividend / numberDivider);
+        System.out.println("Остаток от целочисленного деления: " + numberDividend % numberDivider);
+    }
 }
