d4e3fa3ecd8730dd3034f7d5d85021b8ba72cb1d
Vict Pol <vpolik10@gmail.com>
2022-09-16 21:30:12 +0200

    feat: update Task04 file

--- src/lesson04/Task04.java
+++ src/lesson04/Task04.java
@@ -1,4 +1,20 @@
 package lesson04;
 
 public class Task04 {
+    public static void main(String[] args) {
+        // task - print multiplication table from 1 to 10
+        int numberOfColumn = 1;
+        int i = 0;
+        while (i < 10) {
+            int numberOfRow = 1;
+            int j = 0;
+            while (j < 10) {
+                System.out.println(numberOfColumn * numberOfRow);
+                numberOfRow++;
+                j++;
+            }
+            numberOfColumn++;
+            i++;
+        }
+    }
 }
