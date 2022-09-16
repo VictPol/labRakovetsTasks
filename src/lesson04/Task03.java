e509cc1871193582cdd8e092a55ea715a4408093
Vict Pol <vpolik10@gmail.com>
2022-09-16 21:18:46 +0200

    feat: update Task03 file

--- src/lesson04/Task03.java
+++ src/lesson04/Task03.java
@@ -1,4 +1,17 @@
 package lesson04;
 
 public class Task03 {
+    public static void main(String[] args) {
+        // task - print first ten fibonacci numbers
+        int numberPrevious = 0;
+        int numberNext = 1;
+        System.out.println(numberPrevious + "\n" + numberNext);
+        int j = 2;
+        while (j < 10) {
+            numberPrevious += numberNext;
+            numberNext += numberPrevious;
+            System.out.println(numberPrevious + "\n" + numberNext);
+            j += 2;
+        }
+    }
 }
