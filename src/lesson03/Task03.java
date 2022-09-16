3f7d0495756046757f9867d080bd7b3bf61438f1
Vict Pol <vpolik10@gmail.com>
2022-09-16 20:35:25 +0200

    feat: update Task03 file

--- src/lesson03/Task03.java
+++ src/lesson03/Task03.java
@@ -1,4 +1,18 @@
 package lesson03;
 
 public class Task03 {
+    public static void main(String[] args) {
+        int currentHour = 24;
+        if (currentHour < 0 || currentHour > 24) {
+            System.out.println("Неверно указано значение часа");
+        } else if (currentHour < 6 || currentHour == 24) {
+            System.out.println("Доброй ночи");
+        } else if (currentHour < 12) {
+            System.out.println("Доброе утро");
+        } else  if (currentHour < 18) {
+            System.out.println("Добрый день");
+        } else {
+            System.out.println("Добрый вечер");
+        }
+    }
 }
