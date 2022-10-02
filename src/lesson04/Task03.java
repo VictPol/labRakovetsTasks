8332541ebdc9e400ac80f71e518e148fcc407b7a
Vict Pol <vpolik10@gmail.com>
2022-10-02 10:51:52 +0200

    feat: update Task03 file

--- src/lesson04/Task03.java
+++ src/lesson04/Task03.java
@@ -2,16 +2,16 @@ package lesson04;
 
 public class Task03 {
     public static void main(String[] args) {
-        // task - print first ten fibonacci numbers
         int numberPrevious = 0;
-        int numberNext = 1;
-        System.out.println(numberPrevious + "\n" + numberNext);
-        int j = 2;
-        while (j < 10) {
-            numberPrevious += numberNext;
-            numberNext += numberPrevious;
-            System.out.println(numberPrevious + "\n" + numberNext);
-            j += 2;
+        int numberCurrent = 1;
+        final int TOTAL_NUMBERS_TO_PRINT = 10;
+        int i = 0;
+        while (i < TOTAL_NUMBERS_TO_PRINT) {
+            int numberNext = numberPrevious + numberCurrent;
+            System.out.print(numberPrevious + (i < (TOTAL_NUMBERS_TO_PRINT - 1) ? ", " : ""));
+            numberPrevious = numberCurrent;
+            numberCurrent = numberNext;
+            i++;
         }
     }
 }
