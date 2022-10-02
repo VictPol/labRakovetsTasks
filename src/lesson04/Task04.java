093d31b346d02e31fb00cd47f4d31b2a677acd63
Vict Pol <vpolik10@gmail.com>
2022-10-02 09:56:08 +0200

    feat: update Task04 file

--- src/lesson04/Task04.java
+++ src/lesson04/Task04.java
@@ -2,18 +2,16 @@ package lesson04;
 
 public class Task04 {
     public static void main(String[] args) {
-        // task - print multiplication table from 1 to 10
-        int numberOfColumn = 1;
-        int i = 0;
-        while (i < 10) {
-            int numberOfRow = 1;
-            int j = 0;
-            while (j < 10) {
-                System.out.println(numberOfColumn * numberOfRow);
-                numberOfRow++;
+        final int FIRST_VALUE = 1;
+        final int LAST_VALUE = 10;
+        int i = FIRST_VALUE;
+        while (i <= LAST_VALUE) {
+            int j = FIRST_VALUE;
+            while (j <= LAST_VALUE) {
+                System.out.print(i * j +"\t");
                 j++;
             }
-            numberOfColumn++;
+            System.out.println("");
             i++;
         }
     }
