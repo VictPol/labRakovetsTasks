2acea061b44ccc5db2b1de4d6b91e563d539e688
Vict Pol <vpolik10@gmail.com>
2022-12-04 13:26:10 +0100

    feat: update Task01 file

--- src/lesson01/Task02.java
+++ src/lesson01/Task02.java
@@ -1,7 +1,8 @@
 package lesson01;
 
-public class Task01 {
+public class Task02 {
     public static void main(String[] args) {
         System.out.println("Java: version 11.");
+        System.out.println("JVM: HotSpot.");
     }
 }
