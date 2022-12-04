9482d91b8cf674479d02b21e37f29691556c78b2
Vict Pol <vpolik10@gmail.com>
2022-12-04 13:24:25 +0100

    feat: update Task01 file

--- src/lesson02/Task01.java
+++ src/lesson02/Task01.java
@@ -1,4 +1,10 @@
 package lesson02;
 
 public class Task01 {
+    public static void main(String[] args) {
+        final int X = 5;
+        final int Y = 2;
+        System.out.println(X / Y);
+        System.out.println(X % Y);
+    }
 }
