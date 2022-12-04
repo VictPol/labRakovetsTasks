ebfb4792451a72c75e547d2da2d7758ac18b410c
Vict Pol <vpolik10@gmail.com>
2022-12-04 18:09:40 +0100

    feat: update files for lesson02

--- src/lesson02/Task05.java
+++ src/lesson02/Task05.java
@@ -5,7 +5,7 @@ public class Task05 {
         final float PI = 3.14f;
         final float X = 11.0f;
 
-        System.out.printf("У числа PI есть остаток: %b\n", PI - (int) PI > 0);
-        System.out.printf("У числа Х есть остаток: %b\n", X - (int) X > 0);
+        System.out.printf("У числа PI есть остаток: %b\n", PI > (int) PI);
+        System.out.printf("У числа Х есть остаток: %b\n", X > (int) X);
     }
 }
